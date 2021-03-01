8e14951f6b70310b690060e0dae03b4ac6ed4b68
sujitpal <sujit.pal@comcast.net>
2021-03-01 12:55:11 -0800

    fix: added some more status commands

--- bash-scripts/status.sh
+++ bash-scripts/status.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-docker info | grep "Total Memory"
-echo "Checking if server is up, if response not 200 OK wait a while and retry"
-docker exec vespa bash -c "curl -s --head http://localhost:19071/ApplicationStatus"
+echo "--- Total Memory Available ---"
+sudo docker info | grep "Total Memory"
+echo "--- Config Server State ---"
+sudo docker exec vespa bash -c "curl -s --head http://localhost:19071/ApplicationStatus"
+echo "--- Cluster State ---"
+sudo docker exec vespa bash -c "/opt/vespa/bin/vespa-get-cluster-state"
+echo "--- Proton Status ---"
+sudo docker exec vespa bash -c "/opt/vespa/bin/vespa-proton-cmd --local getProtonStatus"
